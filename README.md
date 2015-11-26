23e774da9088b07bf4bf473b90d997ef5a7265ac
Rui Valente Maia <ruivalentemaia@gmail.com>
2015-11-26 20:34:13 +0000

    Added solution to exercises 3.4 and 3.5.

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ Exercises from the book Functional Programming in Scala (Paul Chiusano, Rúnar B
 * [Exercise 3.1 - Result of match](https://github.com/ruivalentemaia/fpscala/blob/master/singlylinkedlistmatch.scala)
 * [Exercise 3.2 - Function tail that Removes the First Element of a List](https://github.com/ruivalentemaia/fpscala/blob/master/singlyLinkedList.scala)
 * [Exercise 3.3 - Function setHead that Replaces the First Element of a List](https://github.com/ruivalentemaia/fpscala/blob/master/singlyLinkedList.scala)
+* [Exercise 3.4 - Function drop to Remove First n Elements of a List](https://github.com/ruivalentemaia/fpscala/blob/master/singlyLinkedList.scala)
+* [Exercise 3.5 - Function dropWhile to Remove Elements of a List While They Match a Predicate](https://github.com/ruivalentemaia/fpscala/blob/master/singlyLinkedList.scala)
--- singlyLinkedList.scala
+++ singlyLinkedList.scala
@@ -35,4 +35,54 @@ object List {
 		case Cons(Nil, xs) => Cons(elem, xs)
 		case Cons(x,xs) => Cons(elem,xs)
 	}
+
+	/*
+	* Exercise 3.4 - Remove first n elements from
+	* a list, by generalizing tail to this function.
+	*
+	* Test Case:
+	*	val x = List(1,2,3,4,5)
+	*	List.drop(x,2)
+	*
+	* Expected Output:
+	*	List[Int] = Cons(3,Cons(4,Cons(5,Nil)))
+	*/
+	def drop[A](l:List[A], n: Int) : List[A] = {
+		@annotation.tailrec
+		def go(nl: List[A], n:Int) : List[A] = {
+			if(n > 0) go(List.tail(nl),n-1)
+			else nl
+		}
+		if (l == Nil) l
+		else go(l,n)
+	}
+
+	/*
+	* Exercise 3.5 - Removes elements from the List
+	* prefix as long as they match a predicate.
+	*
+	* Test case:
+	* 	val x = List(2,4,6,7,8,9)
+	*	val f = (i:Int) => i % 2 == 0
+	*	List.dropWhile(x,f)
+	*
+	* Expected Output:
+	*	List[Int] = Cons(7,Cons(8,Cons(9,Nil)))
+	*/
+	def dropWhile[A](l: List[A], f: A => Boolean) : List[A] = {
+		@annotation.tailrec
+		def head(nl: List[A]) : A = nl match {
+			case Nil => head(nl)
+			case Cons(x,xs) => x
+		}
+
+		@annotation.tailrec
+		def go(nl: List[A], f: A => Boolean) : List[A] = {
+			if(f(head(nl))) go(drop(nl, 1), f)
+			else nl
+		}
+
+		if(l == Nil) l
+		else go(l,f)
+	}	
 }
