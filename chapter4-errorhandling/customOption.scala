eeed9fa008521d24aa3d598d2251f63ad626e735
Rui Valente Maia <ruivalentemaia@gmail.com>
2016-03-17 19:57:05 +0000

    Fixed Exercise 4.1. Necessary to :paste instead of :load. Added Exercise 4.2. Same thing, :paste instead of :load.

--- chapter4-errorhandling/customOption.scala
+++ chapter4-errorhandling/customOption.scala
@@ -1,27 +1,31 @@
-sealed trait Option[+A] {
+sealed trait Option[+A]{
 	/*
 	*	Exercise 4.1 - Implement functions map, flatMap, getOrElse, orElse
 	*	and filter on Option.
 	*/
 	def map[B](f: A => B) : Option[B] = this match{
 		case None => None
-		case Some(a) = Some(f(a))
+		case Some(a) => Some(f(a))
 	}
 
-	def getOrElse[A,B](B >: A) (default: => B) : B = this match {
+	
+	def getOrElse[B >: A] (default: => B) : B = this match {
 		case None => default
 		case Some(a) => a
 	}
 
-	def flatMap[B](f: A => Option[B]) : Option[B] = 
+	def flatMap[B](f: A => Option[B]) : Option[B] = {
 		map(f) getOrElse None
+	}
 
-	def orElse[A,B](B >: A) (ob: => Option[B]) : Option[B] = 		
+	def orElse[B >: A] (ob: => Option[B]) : Option[B] = {
 		map(Some(_)) getOrElse ob
+	}
 
-	def filter(f: A => Boolean) : Option[A] =
-		flatMap(a: A => if(f(a)) Some(a) else None)
+	def filter(f: A => Boolean) : Option[A] = {
+		flatMap((a: A) => if(f(a)) Some(a) else None)
+	}
 }
 
 case class Some[+A](get: A) extends Option[A]
-case class None extends Option[Nothing]
+case object None extends Option[Nothing]
